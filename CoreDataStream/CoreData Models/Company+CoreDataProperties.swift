cbe7daf424b363deda56a0cf0964449bd0793066
Eduard Galchenko <eddiegalchenko@gmail.com>
2020-02-20 11:30:50 +0200

    added CoreData models

---
+++ CoreDataStream/CoreData Models/Company+CoreDataProperties.swift
@@ -0,0 +1,40 @@
+//
+//  Company+CoreDataProperties.swift
+//  CoreDataStream
+//
+//  Created by Eduard Galchenko on 20.02.2020.
+//  Copyright © 2020 Eduard Galchenko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Company {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Company> {
+        return NSFetchRequest<Company>(entityName: "Company")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var relationship: NSSet?
+
+}
+
+// MARK: Generated accessors for relationship
+extension Company {
+
+    @objc(addRelationshipObject:)
+    @NSManaged public func addToRelationship(_ value: Employee)
+
+    @objc(removeRelationshipObject:)
+    @NSManaged public func removeFromRelationship(_ value: Employee)
+
+    @objc(addRelationship:)
+    @NSManaged public func addToRelationship(_ values: NSSet)
+
+    @objc(removeRelationship:)
+    @NSManaged public func removeFromRelationship(_ values: NSSet)
+
+}
---
+++ CoreDataStream/CoreData Models/Employee+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Employee+CoreDataProperties.swift
+//  CoreDataStream
+//
+//  Created by Eduard Galchenko on 20.02.2020.
+//  Copyright © 2020 Eduard Galchenko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Employee {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Employee> {
+        return NSFetchRequest<Employee>(entityName: "Employee")
+    }
+
+    @NSManaged public var firstName: String?
+    @NSManaged public var lastName: String?
+    @NSManaged public var age: Int16
+    @NSManaged public var relationship: Company?
+
+}
